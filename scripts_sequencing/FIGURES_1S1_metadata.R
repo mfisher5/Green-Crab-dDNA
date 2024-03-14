612690b9778909342f83ba5944cef103ac2c85db
Mary Fisher <mfisher5@uw.edu>
2024-03-13 20:48:36 -0700

    fig code

--- scripts_sequencing/FIGURES_1S1_metadata.R
+++ scripts_sequencing/FIGURES_1S1_metadata.R
@@ -28,14 +28,39 @@ library(ggmap)
 library(janitor)
 library(cowplot)
 
+
+
+
+# Background map ----------------------------------------------------------
+
 register_stadiamaps(skey, write = FALSE)
 
+# myLocation <- c(-124.2, 46, -123.7, 47.4)
+myLocation <- c(-124.5, 46, -123.5, 47.5)
 
-## willapa bay
-myLocation <- c(-124.2, 46, -123.7, 46.8)
+
+## terrain
 mymap <- get_stadiamap(bbox=myLocation,maptype="stamen_terrain_background",crop=FALSE)
-saveRDS(mymap, file=here('data','stamen_map_WillapaBay.rds'))
-mymap <- readRDS(here('data','metadata','stamen_map_WillapaBay.rds'))
+saveRDS(mymap, file=here('data','stamen_map_terrain_wide_WAcoast.rds'))
+mymap <- readRDS(file=here('data','stamen_map_terrain_wide_WAcoast.rds'))
+
+## toner
+mymap <- get_stadiamap(bbox=myLocation,maptype="stamen_toner_background",crop=FALSE)
+saveRDS(mymap, file=here('data','stamen_map_toner_wide_WillapaBay.rds'))
+mymap <- readRDS(here('data','stamen_map_toner_wide_WillapaBay.rds'))
+
+
+attr_mymap <- attr(mymap, "bb")    # save attributes from original
+
+# change background color in raster from black to dark blue
+mymap[mymap == "#000000"] <- "#2f567f"
+
+# correct class, attributes
+class(mymap) <- c("ggmap", "raster")
+attr(mymap, "bb") <- attr_mymap
+ggmap(mymap)  # check map
+
+
 
 # Data --------------------------------------------------------------------
 
@@ -47,7 +72,7 @@ dat <- read_csv(here('data','results','allRuns_BF3_filtered_FINAL_unique_taxa.cs
 
 # site coordinates
 site_coords <- read_csv(here('data/metadata/site_coords.csv')) %>%
-  mutate(type=ifelse(type=="Sentinel","Monitoring",type))
+  mutate(type=ifelse(type=="Sentinel","Slough",type))
 
 # carapace widths
 cw <- read_csv(here('data','metadata','Willapa Bay EGC Samples - Sample Data.csv'))
@@ -58,19 +83,17 @@ cw <- read_csv(here('data','metadata','Willapa Bay EGC Samples - Sample Data.csv
 
 mapdat <- site_coords %>% dplyr::select(site_name,latitude,longitude,type) %>% distinct()
 
-sitemap <- 
-
-ggmap(mymap) +
+sitemap <- ggmap(mymap) +
   geom_point(data=mapdat, aes(x=longitude,y=latitude, col=type), size=6) + 
   geom_label_repel(data=mapdat, aes(x=longitude,y=latitude, label=site_name),
                    segment.colour = 'black',
                    nudge_x=0.22,nudge_y=0.01,
-                   force=1, force_pull=0.2, segment.size=0.05, size=6) +
-  geom_text(aes(x=-123.98,y=46.65,label="italic('Willapa\n Bay')"), col="deepskyblue4", size=4, parse=TRUE) +
-  geom_text(aes(x=-123.87,y=46.23,label="italic('Columbia River')"), col="deepskyblue4", size=4, parse=TRUE) +
+                   force=1, force_pull=0.2, segment.size=0.5, size=6) +
+  geom_text(aes(x=-123.98,y=46.64,label="italic('Willapa\n Bay')"), col="deepskyblue4", size=4, parse=TRUE) +
+  geom_text(aes(x=-123.87,y=46.22,label="italic('Columbia River')"), col="deepskyblue4", size=4, parse=TRUE) +
   ylab("Latitude") + xlab("Longitude") +
   scale_color_manual(values=c("#018571","#a6611a")) +
-  coord_fixed(xlim=c(-124.15,-123.7),ylim=c(46,46.8)) +
+  coord_fixed(xlim=c(-124.15,-123.7),ylim=c(46.15,46.9)) +
   theme(axis.title=element_blank(),
         axis.text=element_text(size=10),
         legend.title=element_blank(),
@@ -127,7 +150,7 @@ plotdat$sequenced <- factor(plotdat$sequenced, levels=c("Trapped","Sequenced","w
 
 plot_CW <- ggplot(plotdat, aes(x=CW_mm, fill=sequenced)) +
   geom_histogram(position="dodge") +
-  scale_fill_manual(values=c("grey60","black","darkred"),name="") +
+  scale_fill_manual(values=c("grey60","black","firebrick3"),name="") +
   facet_grid(rows=vars(site), switch="y") + 
   labs(x="Carapace Width (mm)",y="Number of Green Crab") + 
   theme_bw() + theme(legend.position="top", strip.text=element_text(size=14),legend.text=element_text(size=12),
@@ -152,6 +175,38 @@ dev.off()
 
 
 
+
+
+# Figure S1 ---------------------------------------------------------------
+
+cw.prey <- cw %>% filter(sequenced=="Sequenced w/ Prey")
+cw.sequenced <- cw%>%filter(sequenced %in% c("Sequenced","Sequenced w/ Prey")) %>% mutate(sequenced="Sequenced")
+plotdat <- cw %>% mutate(sequenced="Trapped") %>%
+  bind_rows(cw.sequenced,cw.prey)
+
+plotdat$site  <- factor(plotdat$site, levels=c("Stackpole","Oysterville","Nahcotta","Long Beach"))
+plotdat$sequenced <- factor(plotdat$sequenced, levels=c("Trapped","Sequenced","Sequenced w/ Prey"))
+
+figs1b <- ggplot(plotdat, aes(x=CW_mm, fill=sequenced)) +
+  geom_histogram(position="dodge") +
+  scale_fill_manual(values=c("grey60","black","firebrick3"),name="") +
+  facet_grid(cols=vars(Sex)) + 
+  labs(x="Carapace Width (mm)",y="Number of Green Crab") + theme_bw() + theme(legend.position="top")
+figs1b
+
+figs1a <- plotdat %>% group_by(sequenced,Sex) %>% summarise(ncrab=length(unique(Sample_label))) %>%
+  group_by(sequenced) %>% mutate(totalCrab=sum(ncrab)) %>% ungroup() %>%
+  mutate(pcrab=ncrab/totalCrab) %>%
+  ggplot(aes(x=sequenced,y=pcrab,fill=Sex)) + 
+  geom_col() +
+  scale_fill_manual(values=c("darkolivegreen2","chartreuse4"), name="") +
+  labs(x="",y="Proportion of Green Crab") +
+  theme_bw() + theme(legend.position="top",axis.text.x=element_text(angle=45,hjust=1))
+figs1a
+
+png(here('figs','FigureS1_metadat.png'),res=300, height=1500,width=2100)
+plot_grid(plotlist=list(figs1a,figs1b), rel_widths = c(0.4,1),labels=c("a","b"))
+dev.off()
 
 
 
